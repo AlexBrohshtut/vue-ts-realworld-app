5e42891a2082e1843a524b2623e53b050143d17b
A B <github@brohshtut.com>
2020-03-17 20:39:30 +0200

    feat(layout): header and footer added

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,24 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
+  <div>
+    <vtra-header />
+    <keep-alive>
+      <router-view />
+    </keep-alive>
+    <vtra-footer />
   </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script type="ts">
+import Vue from "vue";
+import Component from "vue-class-component";
+import VtraFooter from "@/components/layouts/Main/VtraFooter";
+import VtraHeader from "@/components/layouts/Main/VtraHeader";
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+@Component({
+  components: {
+    VtraFooter,
+    VtraHeader
+  }
+})
+export default class App extends Vue {}
+</script>
---
+++ src/components/layouts/Main/VtraFooter.vue
@@ -0,0 +1,20 @@
+<template>
+  <footer>
+    <div class="container">
+      <a href="/" class="logo-font">conduit</a>
+      <span class="attribution">
+        An interactive learning project from
+        <a href="https://thinkster.io">Thinkster</a>. Code &amp; design licensed
+        under MIT.
+      </span>
+    </div>
+  </footer>
+</template>
+
+<script type="ts">
+import Vue from "vue";
+import Component from "vue-class-component";
+
+@Component
+export default class VtraFooter extends Vue {}
+</script>
