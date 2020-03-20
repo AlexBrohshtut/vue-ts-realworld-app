f53f2ba43be949d5cffdc9c554c80647c88304e9
A B <github@brohshtut.com>
2020-03-20 19:42:14 +0200

    feat(api): axios added and base api client created

---
+++ src/services/HttpStatusCodes.ts
@@ -0,0 +1,9 @@
+enum HttpStatusCodes {
+  OK = 200,
+  UNAUTHORIZED = 401,
+  FORBIDDEN = 403,
+  NOT_FOUND = 404,
+  UNPROCESSABLE_ENTITY = 422
+}
+
+export default HttpStatusCodes;
---
+++ src/services/realWorldApi/RealWorldApiBase.ts
@@ -0,0 +1,66 @@
+import axios, { AxiosInstance, AxiosResponse, AxiosRequestConfig } from "axios";
+import HttpStatusCodes from "../HttpStatusCodes";
+import router from "@/router";
+import routesNames from "@/router/routesNames";
+
+abstract class RealWorldApiBase {
+  protected readonly Client: AxiosInstance;
+  constructor() {
+    this.Client = axios.create({
+      baseURL: process.env.REAL_WORLD_API_URL,
+      timeout: 5000
+    });
+    this.initializeClientHeaders();
+    this.initializeRequestInterceptors();
+    this.initializeResponseInterceptors();
+  }
+
+  private initializeClientHeaders() {
+    this.Client.defaults.headers.get.Accepts = "application/json";
+    this.Client.defaults.headers.common["Access-Control-Allow-Origin"] = "*";
+  }
+
+  private initializeRequestInterceptors() {
+    this.Client.interceptors.request.use(AuthInterceptor);
+  }
+
+  private initializeResponseInterceptors() {
+    this.Client.interceptors.response.use(OnResponseSuccess, OnResponseFailure);
+  }
+}
+
+const AuthInterceptor = (config: AxiosRequestConfig) => {
+  //TODO: Get real accessToken
+  const accessToken = "";
+  if (accessToken) config.headers.Authorization = `Token ${accessToken}`;
+  return config;
+};
+
+const OnResponseSuccess = (response: AxiosResponse<any>) => response;
+
+const OnResponseFailure = (error: any) => {
+  const httpStatus = error?.response?.status;
+
+  switch (httpStatus) {
+    case HttpStatusCodes.UNAUTHORIZED:
+      //TODO: Logout user here
+      router.push({ name: routesNames.authLogin });
+      //TODO: Notify about not logged it
+      break;
+    case HttpStatusCodes.NOT_FOUND:
+      //TODO: Notify about not found
+      break;
+    case HttpStatusCodes.FORBIDDEN:
+      //TODO: Notify about forbidden
+      break;
+    case HttpStatusCodes.UNPROCESSABLE_ENTITY:
+      //TODO: Notify about UNPROCESSABLE_ENTITY
+      break;
+    default:
+      //TODO: Notify about unknown status code
+      break;
+  }
+  return Promise.reject(error);
+};
+
+export default RealWorldApiBase;
