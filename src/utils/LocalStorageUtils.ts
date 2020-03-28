51b091b1f221ae972c4de26ecf52515204c4519b
A B <github@brohshtut.com>
2020-03-28 15:02:57 +0300

    refactor(local-storage-utils): refactored to avoid unneccessary new instance creation

--- src/utils/LocalStorageUtils.ts
+++ src/utils/LocalStorageUtils.ts
@@ -1,22 +1,22 @@
 import Lockr from "lockr";
 const LOCAL_STORAGE_PREFIX = "__vtra_";
 
-class LocalStorageUtils {
-  constructor() {
-    Lockr.prefix = LOCAL_STORAGE_PREFIX;
-  }
+Lockr.prefix = LOCAL_STORAGE_PREFIX;
 
-  setItem(key: string, value: any): void {
-    Lockr.set(key, value);
-  }
+const setItem = (key: string, value: any): void => {
+  Lockr.set(key, value);
+};
 
-  getItem(key: string): any {
-    return Lockr.get(key);
-  }
+const getItem = (key: string): any => {
+  return Lockr.get(key);
+};
 
-  removeItem(key: string): void {
-    Lockr.rm(key);
-  }
-}
+const removeItem = (key: string): void => {
+  Lockr.rm(key);
+};
 
-export default new LocalStorageUtils();
+export default {
+  setItem,
+  getItem,
+  removeItem
+};
