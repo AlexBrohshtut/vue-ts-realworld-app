f10887c40815c2279a06a04d588fc832ca58812a
A B <github@brohshtut.com>
2020-04-04 14:27:17 +0300

    feat(article-view): article view implemented

---
+++ src/components/ArticleViewActions.vue
@@ -0,0 +1,75 @@
+<template>
+  <article-meta
+    :author-username="author.username"
+    :created-at="article.createdAt"
+    :author-image="author.image"
+  >
+    <template v-if="!isMyArticle">
+      <profile-follow-button
+        :username="author.username"
+        :following="author.following"
+      />
+      &nbsp;&nbsp;
+      <article-favorites-button
+        :favorited="article.favorited"
+        :favorites-count="article.favoritesCount"
+        :slug="article.slug"
+        :is-with-description="true"
+      />
+    </template>
+    <template v-else>
+      <router-link
+        :to="{
+          name: $routesNames.articleEdit,
+          params: { slug: article.slug }
+        }"
+        class="btn btn-outline-secondary btn-sm"
+      >
+        <i class="ion-edit"></i>
+        Edit Article
+      </router-link>
+      &nbsp;&nbsp;
+      <button class="btn btn-outline-danger btn-sm" @click="deleteArticle">
+        <i class="ion-trash-a"></i>
+        Delete Article
+      </button>
+    </template>
+  </article-meta>
+</template>
+
+<script lang="ts">
+import { Component, Emit, Prop, Vue } from "vue-property-decorator";
+
+import ArticleFavoritesButton from "@/components/ArticleFavoritesButton.vue";
+import ArticleMeta from "@/components/ArticleMeta.vue";
+import ProfileFollowButton from "@/components/ProfileFollowButton.vue";
+import { IArticle, IProfile } from "@/services/realWorldApi/models";
+import Profile from "@/store/modules/Profile";
+import User from "@/store/modules/User";
+
+@Component({
+  components: {
+    ArticleMeta,
+    ArticleFavoritesButton,
+    ProfileFollowButton
+  }
+})
+export default class ArticleViewHeader extends Vue {
+  @Prop({ required: true }) article!: IArticle;
+
+  get author(): IProfile {
+    return (
+      Profile.profilesCache[this.article.author.username] || this.article.author
+    );
+  }
+
+  get isMyArticle(): boolean {
+    return this.article.author.username === User.currentUser?.username;
+  }
+
+  @Emit("delete-article")
+  deleteArticle(): void {
+    return;
+  }
+}
+</script>
--- src/views/ArticleView.vue
+++ src/views/ArticleView.vue
@@ -1,10 +1,178 @@
 <template>
-  <div>View article page</div>
+  <common-loader v-if="isLoading || !article" />
+
+  <div v-else class="article-page">
+    <div class="banner">
+      <div class="container">
+        <h1>{{ article.title }}</h1>
+        <article-view-actions
+          :article="article"
+          @delete-article="onDeleteArticle"
+        />
+      </div>
+    </div>
+    <div class="container page">
+      <div class="row article-content">
+        <!-- eslint-disable-next-line vue/no-v-html -->
+        <div class="col-md-12" v-html="HTMLBody"></div>
+      </div>
+
+      <hr />
+
+      <div class="article-actions">
+        <article-view-actions
+          :article="article"
+          @delete-article="onDeleteArticle"
+        />
+      </div>
+
+      <div class="row">
+        <div class="col-xs-12 col-md-8 offset-md-2">
+          <form class="card comment-form">
+            <div class="card-block">
+              <textarea
+                class="form-control"
+                placeholder="Write a comment..."
+                rows="3"
+              ></textarea>
+            </div>
+            <div class="card-footer">
+              <img
+                src="http://i.imgur.com/Qr71crq.jpg"
+                class="comment-author-img"
+              />
+              <button class="btn btn-sm btn-primary">
+                Post Comment
+              </button>
+            </div>
+          </form>
+
+          <div class="card">
+            <div class="card-block">
+              <p class="card-text">
+                With supporting text below as a natural lead-in to additional
+                content.
+              </p>
+            </div>
+            <div class="card-footer">
+              <a href="" class="comment-author">
+                <img
+                  src="http://i.imgur.com/Qr71crq.jpg"
+                  class="comment-author-img"
+                />
+              </a>
+              &nbsp;
+              <a href="" class="comment-author">Jacob Schmidt</a>
+              <span class="date-posted">Dec 29th</span>
+            </div>
+          </div>
+
+          <div class="card">
+            <div class="card-block">
+              <p class="card-text">
+                With supporting text below as a natural lead-in to additional
+                content.
+              </p>
+            </div>
+            <div class="card-footer">
+              <a href="" class="comment-author">
+                <img
+                  src="http://i.imgur.com/Qr71crq.jpg"
+                  class="comment-author-img"
+                />
+              </a>
+              &nbsp;
+              <a href="" class="comment-author">Jacob Schmidt</a>
+              <span class="date-posted">Dec 29th</span>
+              <span class="mod-options">
+                <i class="ion-edit"></i>
+                <i class="ion-trash-a"></i>
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
+import DOMPurify from "dompurify";
+import marked from "marked";
 import { Component, Vue } from "vue-property-decorator";
 
-@Component
-export default class ArticleView extends Vue {}
+import ArticleViewActions from "@/components/ArticleViewActions.vue";
+import CommonLoader from "@/components/CommonLoader.vue";
+import { Location } from "@/router";
+import { IArticle } from "@/services/realWorldApi/models";
+import Article from "@/store/modules/Article";
+
+Component.registerHooks(["beforeRouteEnter", "beforeRouteUpdate"]);
+
+@Component({
+  components: {
+    ArticleViewActions,
+    CommonLoader
+  }
+})
+export default class ArticleView extends Vue {
+  isLoading = false;
+  slug = "";
+  _article: IArticle | null = null;
+
+  get article(): IArticle {
+    return Article.articlesCache[this.slug] || this._article;
+  }
+
+  get HTMLBody(): string {
+    const dirtyHtml = marked(this.article.body);
+    return DOMPurify.sanitize(dirtyHtml);
+  }
+
+  beforeRouteEnter(to: Location, from: Location, next: Function): void {
+    next((vm: ArticleView) => {
+      vm.onRouteUpdate(to, from, next);
+    });
+  }
+  beforeRouteUpdate(to: Location, from: Location, next: Function): void {
+    this.onRouteUpdate(to, from, next);
+  }
+
+  async onRouteUpdate(
+    to: Location,
+    from: Location,
+    next: Function
+  ): Promise<void> {
+    next();
+
+    this.isLoading = true;
+    try {
+      const toSlug = to?.params?.slug;
+      const fromSlug = from?.params?.slug;
+      if (!toSlug) {
+        this.$router.push({ name: this.$routesNames.home });
+        return;
+      }
+      if (toSlug !== fromSlug) {
+        const article = await Article.getSingle(toSlug);
+        this.slug = article.slug;
+      }
+    } catch (e) {
+      //TODO: Error
+      this.$router.push({ name: this.$routesNames.home });
+    } finally {
+      this.isLoading = false;
+    }
+  }
+
+  async onDeleteArticle(): Promise<void> {
+    this.isLoading = true;
+    try {
+      await Article.delete(this.slug);
+      this.$router.push({ name: this.$routesNames.home });
+    } finally {
+      this.isLoading = false;
+    }
+  }
+}
 </script>
