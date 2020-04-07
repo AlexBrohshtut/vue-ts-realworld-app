11fba3c570f930a40e9ebc600072c949bfd9126c
A B <github@brohshtut.com>
2020-04-07 16:56:34 +0300

    feat(github-corner): added

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <github-corner />
     <app-header />
     <keep-alive>
       <router-view />
@@ -12,13 +13,15 @@
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
 
+import GithubCorner from "@/components/GithubCorner.vue";
 import AppFooter from "@/layouts/AppFooter.vue";
 import AppHeader from "@/layouts/AppHeader.vue";
 
 @Component({
   components: {
     AppFooter,
-    AppHeader
+    AppHeader,
+    GithubCorner
   }
 })
 export default class App extends Vue {}
